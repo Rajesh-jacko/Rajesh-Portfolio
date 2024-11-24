81f52297e28270c54eacea7067408111e0108bfb
Rajesh-jacko <rajeshjacko269@gmail.com>
2024-11-25 00:05:09 +0545

    add href

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -12,27 +12,27 @@ function Navigation() {
 
       {/* Center: Clickable Text (Hidden on Mobile) */}
       <div className="hidden md:flex space-x-8">
-        <a href="#project" className="text-xl hover:underline">
+        <a href="#project" className="text-xl nav-btn">
           Projects
         </a>
-        <a href="#technologies" className="text-xl hover:underline">
+        <a href="#technologies" className="text-xl nav-btn">
            Technologies
         </a>
-        <a href="#about-me" className="text-xl hover:underline">
+        <a href="#about-me" className="text-xl nav-btn">
           About Me
         </a>
       </div>
 
       {/* Right Side: Social Media Icons */}
       <div className="flex space-x-4">
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          <FaInstagram className="text-2xl" />
+        <a href="https://www.instagram.com/rajes_jyakho/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram className="text-2xl nav-btn" />
         </a>
-        <a href="https://github.com" target="_blank" rel="noopener noreferrer">
-          <FaGithub className="text-2xl" />
+        <a href="https://github.com/Rajesh-jacko" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="text-2xl nav-btn" />
         </a>
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          <FaFacebook className="text-2xl" />
+        <a href="https://www.facebook.com/Minijay12/" target="_blank" rel="noopener noreferrer">
+          <FaFacebook className="text-2xl nav-btn" />
         </a>
       </div>
     </div>
